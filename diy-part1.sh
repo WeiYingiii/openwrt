28ee2f8d4092642ea54a0f0a9f0bd2e0ead255c5
WeiYingiii <1489185654@qq.com>
2024-12-27 13:26:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,3 +10,4 @@ sed -i "s/src-git-full-full/src-git-full/g" feeds.conf.default
 
 # 添加feeds源
 #echo 'src-git-full helloworld https://github.com/fw876/helloworld.git;main' >> feeds.conf.default
+echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
